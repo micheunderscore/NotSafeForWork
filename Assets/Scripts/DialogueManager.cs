3e4301c9689c81249a0aa33f4248ae9643cb2772
Miche_ <51939599+micheunderscore@users.noreply.github.com>
2023-01-25 01:59:45 +0800

    feat: dialogue engine now has basic functionality

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -9,25 +9,83 @@ using TMPro;
 public class DialogueManager : MonoBehaviour {
     [SerializeField]
     private TMP_Text DialogueBox;
+    [SerializeField]
+    private Transform ChoiceBox;
+    [SerializeField]
+    private GameObject ChoiceButtonPrefab;
     private JsonReader jsonReader = new JsonReader();
+    private Transformer t = new Transformer();
     private Chapter chapter;
-    private string currDialogue;
-    private Scene[] scenes;
-    private Queue<string> dialogues;
+    private Dialogue currentDialogue;
+    private Queue<Dialogue> dialogues;
+    private Queue<Scene> scenes;
+    private Queue<string> speeches;
+    private Reply[] currentReplies;
+    private string currentSpeech;
+    private Scene currentScene;
+    private DialogueState currentState;
 
     public void Start() {
-        string jsonString = jsonReader.Read("chapters" + Path.DirectorySeparatorChar + "00_example_chapter.json");
-        chapter = JsonUtility.FromJson<Chapter>(jsonString);
-        scenes = Array.Find(chapter.dialogues, dialogue => dialogue.id == chapter.head).scenes;
-        dialogues = new Queue<string>(scenes[0].speech);
-        currDialogue = dialogues.Dequeue();
+        InitializeDialogue();
     }
 
-    // TODO: Create method to hierarchically dequeue speeches depending on if current queue is empty.
+    public void InitializeDialogue(string dialogueId = null) {
+        // FIXME: This currently doesn't support chapters where it starts with replies
+        string jsonString = jsonReader.Read("chapters" + Path.DirectorySeparatorChar + "00_example_chapter.json");
+        chapter = JsonUtility.FromJson<Chapter>(jsonString);
+
+        dialogues = t.ToDialogueQueue(chapter.dialogues);
+
+        currentDialogue = Array.Find(chapter.dialogues, dialogue => dialogue.id == (dialogueId ?? chapter.head));
+        scenes = t.ToSceneQueue(currentDialogue.scenes);
+        currentScene = scenes.Dequeue();
+
+        speeches = t.ToStringQueue(currentScene.speech);
+        currentSpeech = speeches.Dequeue();
+
+        currentState = DialogueState.SPEECH;
+        DestroyChoices();
+
+        Debug.Log(JsonUtility.ToJson(chapter));
+        Debug.Log(jsonString);
+    }
 
     public void Update() {
-        DialogueBox.SetText(currDialogue);
-        if (Input.GetMouseButtonDown(0))
-            currDialogue = dialogues.Dequeue();
+        DialogueBox.SetText(currentSpeech);
+        if (currentState != DialogueState.CHOICE) {
+            if (Input.GetMouseButtonDown(0)) currentSpeech = NextDialogue();
+        }
+    }
+
+    public string NextDialogue() {
+        while (true) {
+            if (currentScene.end != null) {
+                return "End of Chapter";
+            } else if (speeches.TryDequeue(out string dequeuedSpeech)) {
+                return dequeuedSpeech;
+            } else if (scenes.TryDequeue(out Scene dequeuedScene)) {
+                currentScene = dequeuedScene;
+                if (currentScene.speech != null) speeches = t.ToStringQueue(currentScene.speech);
+            } else {
+                currentState = DialogueState.CHOICE;
+                foreach (Reply reply in currentDialogue.replies) {
+                    GameObject choiceButton = Instantiate(ChoiceButtonPrefab, parent: ChoiceBox);
+                    choiceButton.GetComponent<ChoiceButton>().next = reply.next;
+                    choiceButton.GetComponent<ChoiceButton>().option = reply.option;
+                }
+                return "Answer the Question";
+            }
+        };
+    }
+
+    public void DestroyChoices() {
+        foreach (GameObject choice in GameObject.FindGameObjectsWithTag("Choice")) Destroy(choice, 0.1f);
+    }
+
+    private enum DialogueState {
+        SPEECH,
+        CHOICE,
+        CUTSCENE,
+        END
     }
 }
--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class JsonReader {
@@ -17,5 +18,27 @@ public class JsonReader {
 #endif
 
         return (jsonString);
+    }
+}
+
+public class Transformer {
+    public Queue<string> ToStringQueue(IEnumerable<string> arr) {
+        Queue<string> queue = new Queue<string>(arr);
+        return queue;
+    }
+
+    public Queue<Scene> ToSceneQueue(IEnumerable<Scene> arr) {
+        Queue<Scene> queue = new Queue<Scene>(arr);
+        return queue;
+    }
+
+    public Queue<Dialogue> ToDialogueQueue(IEnumerable<Dialogue> arr) {
+        Queue<Dialogue> queue = new Queue<Dialogue>(arr);
+        return queue;
+    }
+
+    public Queue<Reply> ToReplyQueue(IEnumerable<Reply> arr) {
+        Queue<Reply> queue = new Queue<Reply>(arr);
+        return queue;
     }
 }
