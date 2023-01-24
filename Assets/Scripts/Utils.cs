5cff1fd4c6911b012fbc8930455d38a46c1324d5
Miche_ <51939599+micheunderscore@users.noreply.github.com>
2023-01-24 18:17:06 +0800

    feat: added json reader util

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using UnityEngine;
+
+public class JsonReader
+{
+    public string Read(string route)
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath + Path.DirectorySeparatorChar, route);
+        string jsonString = "";
+
+#if UNITY_EDITOR || UNITY_IOS
+        jsonString = File.ReadAllText(filePath);
+
+#elif UNITY_ANDROID
+        WWW reader = new WWW (filePath);
+        while (!reader.isDone) {
+        }
+        jsonString = reader.text;
+#endif
+
+        return (jsonString);
+    }
+
+    public string GetUsername()
+    {
+        return (PlayerPrefs.GetString("username", "Mel"));
+    }
+}
